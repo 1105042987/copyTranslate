c7774801df1a202973f1e5f7729d523003fd1b46
Sufer_Qin <1105042987@qq.com>
2021-06-30 14:16:53 +0800

    change the way to install pyhook3

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ import os
 import os.path as p
 import sys
 
+base = p.dirname(p.abspath(__file__))
+
 if len(sys.argv)>1 and sys.argv[1] == 'install':
     os.system('python -m pip install --upgrade pip')
-    os.system('conda install swig')
-    os.system('pip install pyperclip PyExecJS PyHook3')
+    os.system('pip install pyperclip PyExecJS')
+    os.system('pip install '+p.join(base,'PyHook3-1.6.1-cp37-cp37m-win_amd64.whl'))
 
-base = p.dirname(p.abspath(__file__))
 with open(p.join(base,'start.bat'),'w') as f:
+    f.write('CALL '+ p.join(sys.path[4],'Scripts','activate.bat')+' '+sys.path[4]+'\n')
     f.write('start '+p.join(sys.path[4],'python')+' '+p.join(base,'copyTranslate.py'))
